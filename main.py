3b7e12a7e1f719c40bfc5a10231822e9d79cf8b6
blackyaksha <101398596+blackyaksha@users.noreply.github.com>
2022-03-11 20:37:05 +0800

    update main.py
    main.py update with menu,, may ieedit pa diyan ung may #ongoing edit

--- main.py
+++ main.py
@@ -28,12 +28,18 @@ class LogInWindow(): # login window
 
         self.menu1 = Menu(login)
         login.config(menu=self.menu1)
+        
+        def surequit():
+            if messagebox.askyesno("Verify", "Are you sure you want to quit?"):
+                exit()
+            else:
+                messagebox.showinfo("Return", "User will return to the Login Menu.")
 
         file_menu = Menu(self.menu1)
         self.menu1.add_cascade(label="Options", menu=file_menu)
         file_menu.add_command(label="Help", command=HelpWindow)
-        file_menu.add_command(label="Quit",  command=login.quit)
-
+        file_menu.add_command(label="Quit",  command=surequit)
+        
         self.text = Label(login, text="SetPy", font=('Forte', 34), fg='black', bg='#FFD39B', width=60)
         self.text.pack(ipady=50, pady=10, padx=10)
 
@@ -54,19 +60,62 @@ class LogInWindow(): # login window
 
 
         login.mainloop()
-
-class NewMain():
+        
+class NewMain(Frame):
 
     def __init__(self):
         newmain = Tk()
+        menu = Menu(newmain)
+        newmain.config(menu=menu)
+
+        fileMenu = Menu(menu)
+        fileMenu.add_command(label="Logout", command=newmain.destroy)
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Exit", command=self.exitProgram)
+        menu.add_cascade(label="File", menu=fileMenu)
+
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Undo")
+        editMenu.add_command(label="Redo")
+        menu.add_cascade(label="Edit", menu=editMenu)
+        
+        viewplannMenu = Menu(menu)
+        viewplannMenu.add_command(label = "View Plan")
+        menu.add_cascade(label = "View Plan", menu=viewplannMenu)
+            
+        helpMenu = Menu(menu)
+        helpMenu.add_command(label = "Help", command = HelpWindow)
+        helpMenu.add_separator()
+        helpMenu.add_command(label = "About", command = self.about_popbox)
+        menu.add_cascade(label = "Help", menu=helpMenu)
+        
         newmain.title("SetPy App")
         newmain.geometry("700x500")
         newmain.mainloop()
+        
+    def about_popbox(self):
+        about_input =  "SETPy is an application that tracks and evaluate your sleeping routine.\
+            \nIt will help you to track your sleeping hours and evaluate it if they are healthy or not.\
+            \n Windows 10\
+            \n Copyright 2022 Final Project 5"
+        messagebox.showinfo("SETPy About", about_input)
+    
+    def exitProgram(self):
+        exit()
 
 class HelpWindow():
 
-    def __init__(self):
-        help = Tk()
+    def __init__(self): 
+        help = Tk() #ongoing edit 
+        help_input =  "1. Raiden Shogun\
+            \n2. Shenhe\
+            \n3. Ningguang\
+            \n5. Kaedahara Kazuha\
+            \n6. Venti\
+            \n7. Kamisato Ayato"
+        msg = Message(help, text = help_input)
+        msg.config(justify= "left", font= ('arial', 22))
+        msg.pack()
         help.title("Help")
         help.geometry("400x500")
         help.mainloop()
